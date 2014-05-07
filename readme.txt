a1f05de556f3ceb650f631e7f3081c141044481c
sse10 <sse10@aber.ac.uk>
2014-05-07 14:28:48 +0100

    minor update to readme.txt, wrote main method for Test.java

--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
-This is an example Git repository.
+This is another example Git repository.
+Veni, vidi, vici.
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,7 @@
-line 1
-line 2 ...
+class Test {
+
+   public static void main(String[] args) {
+      System.out.println("Hi Git!");
+   }
+
+}
